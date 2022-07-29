62ee5972272f40250a474e8cc0250b0c61b731c8
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2022-07-29 10:53:11 +0300

    chore: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+@import "./index.css";
+</style>
